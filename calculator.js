735bae90b84a9eb601747a56f6cc13977f181f70
Darma <meet.darma@gmail.com>
2025-04-23 17:50:40 +0700

    edit css and html layout

--- calculator.js
+++ calculator.js
@@ -1,1 +1,32 @@
-console.log('hi');
+
+// Declaring variables
+let currentDisplay = '0';
+let previousDisplay = '';
+let resetDisplay = false;   // flag
+let operation = null;
+
+// Get elements
+const display = document.getElementById('display');
+const button = document.querySelectorAll('button');
+
+    button.forEach(button => {
+        const value = button.textContent;
+        if (['x', '÷', '+', '-'].includes(value)) {
+            button.onclick = () => {
+                setOperation(value);
+            }
+        }
+    })
+
+function setOperation() {
+
+}
+
+function updateDisplay() {
+    display.textContent = 'hi';
+}
+
+function appendDisplay() {
+    console.log('button');
+    
+}
