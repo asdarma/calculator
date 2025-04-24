00a5a74320eca36c042ea1bbed6696853e192ec8
Darma <meet.darma@gmail.com>
2025-04-24 10:00:49 +0700

    done feature calculate()

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 
 // Declaring variables
-let currentDisplay = '0';
-let previousDisplay = '';
-let resetDisplay = false;   // flag
-let operation = null;
+let currentInput = '0';
+let nextInput = '';
+let operator = null
+let resetFlag = false;
 
 // Get elements
 const display = document.getElementById('display');
@@ -20,28 +20,50 @@ const button = document.querySelectorAll('button');
 
 // Handle operation
 function setOperation(value) {
-    if (operation !== null) calculate();
-    previousDisplay = currentDisplay;
-    operation = value;
-    resetDisplay = true;
+    if (operator !== null) calculate();
+    nextInput = currentInput;
+    operator = value;
+    resetFlag = true;
 }
 
 function updateDisplay() {
-    display.textContent = currentDisplay;
+    display.textContent = currentInput;
 }
 
-function appendDisplay() {
-    console.log('number');
-    
+function appendDisplay(value) {
+    if (currentInput === '0' || resetFlag) {
+        currentInput = '';
+        resetFlag = false;
+    }
+    if (value === '.' && currentInput.includes('.')) {
+        return;
+    }
+    currentInput += value;
+    updateDisplay();
 }
 
 function calculate() {
-    console.log('calc')
+    let result;
+    num1 = parseFloat(currentInput);
+    num2 = parseFloat(nextInput);
+
+    switch (operator) {
+    case'x': result = num1 * num2; break;
+    case'÷': result = num1 / num2; break;
+    case'+': result = num1 + num2; break;
+    case'-': result = num1 - num2; break;
+    default: return;
+    }
+
+    currentInput = result
+    operator = null;
+    updateDisplay();
 }
 
 function clearDisplay() {
-    currentDisplay = '0';
-    previousDisplay = '';
-    resetDisplay = false;
-    operation = null;
+    currentInput = '0';
+    nextInput = '';
+    operator = null;
+    resetFlag = false;
+    updateDisplay();
 }
